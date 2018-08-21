1215349417273b3ae65ce4045f4f34f02468465f
ohad <ohad111@gmail.com>
2018-08-22 01:39:41 +0300

    added: mobile navigation bar and js navigation controls

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', async () => {
+    let buttons = document.getElementsByTagName("li");
+    for (button of buttons) {
+        button.addEventListener('click', (event) => {
+            if (!event.target.classList.contains('active')) {
+                let oldActive = document.querySelectorAll('li.active');
+                let newActive = document.querySelectorAll('li.' + event.target.className);
+                let element = document.querySelector('.content').getElementsByClassName('active');
+                let targetEl = document.querySelector('.content').getElementsByClassName(event.target.className);
+
+                element[0].classList.replace('active','hidden');
+                targetEl[0].classList.replace('hidden','active');
+
+                for (let i = 0; i < oldActive.length; i++ ) {
+                    oldActive[i].classList.remove('active');
+                    newActive[i].classList.add('active');
+                }
+            }
+
+        });
+    }
     let canvas = SVG('drawing').size('100%', '100%').viewbox(0,0,640,480);
-    let svgAnimations = await illustrator(canvas, starSystem).then(async (result) => {
+    let svgAnimations = await illustrator(canvas, soccerPitch).then(async (result) => {
         let animated = await animator(result);
         return animated
     });
