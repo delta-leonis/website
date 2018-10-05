520ce4897caa3288e471e6d86f65ca2463caa947
Jeroen de Jong <thumbnail95@gmail.com>
2018-10-05 10:50:45 +0200

    revert some reformatting changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,8 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  canvas = SVG("svg-container")
-    .size("90%", "90%")
-    .viewbox(0, 0, 640, 480);
+  canvas = SVG("svg-container").size("90%", "90%").viewbox(0, 0, 640, 480);
 
-  particlesJS.load(
-    "particles-js",
-    "assets/js/vendor/particlesjs-config.json",
-    () => {
-      document.getElementById("particles-js").style.animation =
-        "show 3s forwards";
-    }
+  particlesJS.load("particles-js", "assets/js/vendor/particlesjs-config.json", () =>
+      document.getElementById("particles-js").style.animation = "show 3s forwards"
   );
 
   window.onpopstate = () => selectPage(canvas, window.location.hash);
@@ -20,19 +13,14 @@ function selectPage(canvas, hash) {
   id = hash.substring(1);
 
   // remove existing active classes and hide all
-  document
-    .querySelectorAll(".active")
-    .forEach(element => element.classList.remove("active"));
-  document
-    .querySelectorAll("#content div")
-    .forEach(element => element.classList.add("hidden"));
+  document.querySelectorAll(".active").forEach(element => element.classList.remove("active"));
+  document.querySelectorAll("#content div").forEach(element => element.classList.add("hidden"));
 
   // add .active to the new active item and content
   content = document.getElementById(id)
   content.classList.remove("hidden");
   content.classList.add("active");
-  document.querySelector('a[href="' + hash + '"]')
-          .parentElement.classList.add("active");
+  document.querySelector('a[href="' + hash + '"]').parentElement.classList.add("active");
 
   clearCanvas(canvas);
   animator(illustrator(canvas, window[content.dataset.animation]));
