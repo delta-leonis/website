e7a371e509d77a519ed348e98f25586fb3293ef6
Jeroen de Jong <thumbnail95@gmail.com>
2018-10-05 14:03:34 +0200

    Revert code to ES2015, now IE compatible

--- assets/js/svgbuilder.js
+++ assets/js/svgbuilder.js
@@ -1,4 +1,5 @@
-function illustrator(canvas, blueprint = []){
+function illustrator(canvas, blueprint){
+  blueprint = blueprint || []
   let shapeRef = [],
       animationRef = {};
 
@@ -14,7 +15,7 @@ function illustrator(canvas, blueprint = []){
                       animationRef[animationName] = group;
                   }
               }
-              shapeRef.push({group});
+              shapeRef.push(group);
           }
       }
   }
@@ -60,33 +61,43 @@ function animator(blueprint) {
               break;
       }
   }
-  return {animationRef,blueprint};
 }
 
-function drawShapes(canvas, shapeName, { data: shapes, opt}) {
-  return shapes.reduce((group, shape) => group.add(drawFunctions[shapeName](canvas, shape, opt)), canvas.group());
+function drawShapes(canvas, shapeName, shapes) {
+  return shapes.data.reduce(function(group, shape) {
+    return group.add(drawFunctions[shapeName](canvas, shape, shapes.opt))
+  }, canvas.group());
 }
 
 const drawFunctions = {
-  lines: (canvas, line, { stroke }) =>
-    canvas.line(line.x,line.y,line.x1,line.y1).stroke(stroke),
-  polygons: (canvas, poly, { stroke = 'none', fill = 'none' }) =>
-    canvas.polygon(poly).stroke(stroke).fill(fill),
-  ellipses: (canvas, ellipse, {stroke, fill}) =>
-    canvas.ellipse().radius(ellipse.rx,ellipse.ry).center(ellipse.cx,ellipse.cy).stroke(stroke).fill(fill),
-  paths: (canvas, path, { stroke = 'none', fill = 'none'}) =>
-    canvas.path(path).stroke(stroke).fill(fill),
-  circles: (canvas, circle, { stroke = 'none', fill = 'none' }) => {
-    circleRef = canvas.circle(circle.r).stroke(stroke).fill(fill);
+  lines: function(canvas, line, data) {
+    return canvas.line(line.x,line.y,line.x1,line.y1).stroke(data.stroke)
+  },
+  polygons: function(canvas, poly, data) {
+    return canvas.polygon(poly).stroke(data.stroke || 'none').fill(data.fill || 'none')
+  },
+  ellipses: function(canvas, ellipse, data) {
+    return canvas.ellipse().radius(ellipse.rx,ellipse.ry).center(ellipse.cx,ellipse.cy).stroke(data.stroke).fill(data.fill)
+  },
+  paths: function(canvas, path, data) {
+    return canvas.path(path).stroke(data.stroke || 'none').fill(data.fill || 'none')
+  },
+  circles: function(canvas, circle, data) {
+    circleRef = canvas.circle(circle.r).stroke(data.stroke || 'none').fill(data.fill || 'none');
     if (circle.hasOwnProperty('cx')) {
-        circleRef.center(circle.cx,circle.cy);
+        circleRef.center(circle.cx, circle.cy);
     }
     return circleRef;
   }
 };
 
 function pack(oldRef, newRef) {
-  return {[oldRef.node.id]:oldRef, [newRef.node.id]:newRef}
+  id1 = oldRef.node.id
+  id2 = newRef.node.id
+  return {
+    id1: oldRef,
+    id2: newRef
+  }
 }
 
 function getRandomNum(min, max) {
@@ -105,8 +116,9 @@ function getRandomColor() {
 function animateOtherPlayer(target) {
   let flip = false;
   for (let group in target) {
-      for (let child of target[group].children()) {
-          child.fill('#000000').animate(1000).fill('#fff');
+      children = target[group].children();
+        for (let i = 0; i < children.length; i++) {
+          children[i].fill('#000000').animate(1000).fill('#fff');
       }
       if (!flip) {
           target[group].animate(7000, 'swingTo' , 500).move(25,-25).loop(true,true);
@@ -115,20 +127,21 @@ function animateOtherPlayer(target) {
           target[group].animate(5000, 'swingTo' , 500).move(25,15).loop(true,true);
       }
   }
-  return target
+  return target;
 }
 
 function animateMainPlayer(target) {
   target.children()[0].fill('#000').scale(0.1).animate(1500, 'elastic').scale(1).fill('#fff');
-  return target
+  return target;
 }
 
 function animatePlot(target) {
-  for (let child of target.children()) {
-      child.fill('#000').animate(1400).fill('#fff');
+  let children = target.children();
+  for (let i = 0; i < children.length; i++) {
+      children[i].fill('#000').animate(1400).fill('#fff');
   }
   target.animate(3000, 'elastic').skew(10, 0);
-  return target
+  return target;
 }
 
 function animateStars(target, id) {
@@ -141,50 +154,54 @@ function animateStars(target, id) {
       if (type === 'circle') {
           starRef[type] = {sun: childArray.shift(), moon:childArray.pop(), data:childArray};
 
-          for (let child of target[group].children()) {
-              let original = child.attr('r');
-              child.attr({'r': 0.1}).animate(getRandomNum(1000,1500), 'elastic').attr({'r': original});
+          children = target[group].children();
+          for (let i = 0; i < children.length; i++) {
+              let original = children[i].attr('r');
+              children[i].attr({'r': 0.1}).animate(getRandomNum(1000,1500), 'elastic').attr({'r': original});
           }
       } else {
           starRef[type] = {moonPath:childArray.pop(), data:childArray};
-          for (let child of target[group].children()) {
-              child.scale(0.1).stroke({width:0, color: '#000000'}).animate(getRandomNum(1000,1500), 'elastic').scale(1).stroke({width:0.4, color: '#fff'});
+
+          children = target[group].children();
+          for (let i = 0; i < children.length; i++) {
+              children[i].scale(0.1).stroke({width:0, color: '#000000'}).animate(getRandomNum(1000,1500), 'elastic').scale(1).stroke({width:0.4, color: '#fff'});
           }
       }
   }
-  setTimeout(() => {
+  setTimeout(function() {
       for (let i = 0; i < starRef.circle.data.length; i++) {
           duration += getRandomNum(10000,25000);
           let length = starRef.path.data[i].length();
-          starRef.circle.data[i].animate(duration).during((pos, morph, eased) =>{
+          starRef.circle.data[i].animate(duration).during(function(pos, morph, eased) {
               let p = starRef.path.data[i].pointAt(eased * length);
               starRef.circle.data[i].center(p.x, p.y);
           }).loop(true);
           if (i === 3) {
 
-              starRef.circle.moon.animate(8000).during((pos, morph, eased) => {
+              starRef.circle.moon.animate(8000).during(function(pos, morph, eased) {
                   let l = starRef.path.moonPath.length();
                   let p = starRef.path.moonPath.pointAt(eased * l);
                   starRef.circle.moon.center(p.x, p.y);
               }).loop(true);
-              starRef.path.moonPath.animate(duration).during((pos, morph, eased) => {
+              starRef.path.moonPath.animate(duration).during(function(pos, morph, eased) {
                   let p = starRef.path.data[i].pointAt(eased * length);
                   starRef.path.moonPath.center(p.x, p.y);
               }).loop(true);
           }
       }
-      return target
-  },2500)
+      return target;
+  }, 2500);
 }
 
 function animateGraph(target) {
   let lines = target[Object.keys(target)[0]];
   let paths = target[Object.keys(target)[1]];
   let vPath = new Vivus(paths.node.id, {type: 'sync', duration: 100, start: 'autostart', animTimingFunction: Vivus.EASE_OUT});
-  let vLine = new Vivus(lines.node.id, {type: 'delayed', duration: 75, start: 'autostart', animTimingFunction: Vivus.EASE_OUT}, () => {
-      for (let line of lines.children()) {
-          line.animate(getRandomNum(1000,3000),'<>', 500).stroke({width: 2, color: '#333333'}).loop(true,true);
+  let vLine = new Vivus(lines.node.id, {type: 'delayed', duration: 75, start: 'autostart', animTimingFunction: Vivus.EASE_OUT}, function() {
+      children = lines.children();
+      for (let i = 0; i < children.length; i++) {
+          children[i].animate(getRandomNum(1000,3000),'<>', 500).stroke({width: 2, color: '#333333'}).loop(true,true);
       }
-      return target
+      return target;
   });
 }
