5bda7b3635e3680cbef49f1aacd0f35168c5976e
Jeroen de Jong <thumbnail95@gmail.com>
2018-10-08 16:37:46 +0200

    Clean js up a bit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  canvas = SVG("svg-container").size("90%", "90%").viewbox(0, 0, 640, 480);
+  let canvas = SVG("svg-container").size("90%", "90%").viewbox(0, 0, 640, 480);
 
   particlesJS.load("particles-js", "assets/js/vendor/particlesjs-config.json", () =>
-      document.getElementById("particles-js").style.animation = "show 3s forwards"
-  );
+    document.getElementById("particles-js").style.animation = "show 2s forwards");
 
   window.onpopstate = () => selectPage(canvas, window.location.hash);
   selectPage(canvas, window.location.hash);
 });
 
 function selectPage(canvas, hash) {
-  id = hash.substring(1);
-
-  if(container = document.getElementById(id)) {
-    if(container.classList.contains('active')) {
-      return;
-    }
-  } else {
-    id = 'home';
-  }
+  let container = document.getElementById(hash.substring(1)) || document.getElementById("home");
+  let id = container.id;
+  if (container.classList.contains("active")) return;
 
   // remove existing active classes and hide all
-  for(element of document.querySelectorAll(".active")) {
+  for (let element of document.querySelectorAll(".active")) {
     element.classList.remove("active");
   }
-  for(element of document.querySelectorAll("#content div")){
+  for (let element of document.querySelectorAll("#content div")) {
     element.classList.add("hidden");
   }
 
   // add .active to the new active item and content
-  content = document.getElementById(id)
+  let content = document.getElementById(id);
   content.classList.remove("hidden");
   content.classList.add("active");
-  document.querySelector('a[href="#' + id + '"]').parentElement.classList.add("active");
+  document.querySelector("a[href=\"#" + id + "\"]").parentElement.classList.add("active");
 
   clearCanvas(canvas);
   animator(illustrator(canvas, window[content.dataset.animation]));
